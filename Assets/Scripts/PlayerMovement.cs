fe4974a93aa3ce08c7d96085579efb74764f96f3
cloudyboxscape <reder.joubert@reda.za.net>
2025-08-11 20:16:00 +0200

    Player Movement
    I added a lovely flair that gives a delay to the movement of the camera.
    Andale tested and does get nauseous. I can add a button that says "no lerp" so that people don't want to get nauseous.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Needed for new Input System
+using static UnityEngine.GraphicsBuffer;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] Transform _cameraFPS;
+    [SerializeField, Range(0, 15f)] float _mouseSensitivity;
+    [SerializeField, Range(0, 0.1f)] float _mouseLerpSpeed;
+    [SerializeField, Range(0, -90f)] float _clampUp;
+    [SerializeField, Range(0f, 90f)] float _clampDown;
+    [SerializeField, Range(0f, -90f)] float _clampLeft;
+    [SerializeField, Range(0f, 90f)] float _clampRight;
+    float _rotationX = 0f;
+    float _rotationY = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        getMouseMovement();
+    }
+
+    public void getMouseMovement()
+    {
+        if (Mouse.current != null)
+        {
+            Vector2 mouseDelta = Mouse.current.delta.ReadValue() * _mouseSensitivity * Time.deltaTime;
+
+            _rotationX -= mouseDelta.y;
+            _rotationX = Mathf.Clamp(_rotationX, _clampUp, _clampDown);
+            _rotationY += mouseDelta.x;
+            _rotationY = Mathf.Clamp(_rotationY, _clampLeft, _clampRight);
+
+            Quaternion targetRotation = Quaternion.Euler(_rotationX, _rotationY, 0f);
+            //lerp
+            _cameraFPS.localRotation = Quaternion.Slerp(_cameraFPS.localRotation, targetRotation, _mouseLerpSpeed);
+        }
+    }
+}
