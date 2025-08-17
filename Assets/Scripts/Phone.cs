71253a3ff2a5de34f4db22feef4c40a8049e8817
cloudyboxscape <reder.joubert@reda.za.net>
2025-08-17 14:06:29 +0200

    Camera Lerps to the Phone and Back
    I have made an interactive for the phone. This is just a test for the time bein I will make a plan to make a better Interactive when you are done with the phone xD

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -1,13 +1,67 @@
+using TMPro;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Phone : MonoBehaviour, IInteractable
 {
-    
+    [SerializeField] GameObject _playerMainCamera;
+    [SerializeField] GameObject _CharacterRealParent;
+    [SerializeField, Range(0,5)] float _camerSpeed;
+    private bool _interacted = false;
+
+    private void Start()
+    {
+        _playerMainCamera = GameObject.Find("Main Camera");
+        _CharacterRealParent = GameObject.Find("Character");
+    }
     public void interact()
     {
         //what happens when the player interacts with phone (from phones perspective)
-        //play animation to players face
-        Debug.Log("Phone is Interacted");
+        //set the camera parent of the phone
+        if(_interacted == false)
+        {
+            _playerMainCamera.transform.SetParent(this.transform);
+            _interacted = true;
+            //this.GetComponent<Collider>().enabled = false;
+            Debug.Log("Phone is Interacted");
+        }
+        else
+        {
+            _playerMainCamera.transform.SetParent(_CharacterRealParent.transform);
+            _interacted = false;
+            //this.GetComponent<Collider>().enabled = true;
+            Debug.Log("Phone is Exited");
+        }
+       
+
+
+    }
+
+    private void LateUpdate()
+    {
+        if (_interacted == true)
+        {
+            //set the transorm to lerp till it is Vector3(0, 2, -0.3f)
+            Vector3 targetPosition = new Vector3(0, 2, -0.3f);
+            _playerMainCamera.transform.localPosition = Vector3.Lerp(_playerMainCamera.transform.localPosition, targetPosition, _camerSpeed * Time.deltaTime); // 5 = speed factor, adjust as needed
+            if (Vector3.Distance(_playerMainCamera.transform.localPosition, targetPosition) < 0.01f)
+            {
+                _playerMainCamera.transform.localPosition = targetPosition; // snap exactly
+                //this.enabled = false; // disable script
+            }
+        }
+        else
+        {
+            //set the transorm to lerp till it is Vector3(0, 2, -0.3f)
+            Vector3 targetPosition = new Vector3(0, 1.4f, 0);
+            _playerMainCamera.transform.localPosition = Vector3.Lerp(_playerMainCamera.transform.localPosition, targetPosition, _camerSpeed * Time.deltaTime); // 5 = speed factor, adjust as needed
+            if (Vector3.Distance(_playerMainCamera.transform.localPosition, targetPosition) < 0.01f)
+            {
+                _playerMainCamera.transform.localPosition = targetPosition; // snap exactly
+                //this.enabled = false; // disable script
+            }
+        }
+        
     }
 
 }
